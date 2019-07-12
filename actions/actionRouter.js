0b05b30647d0372227f6f5f45e1955d781e3d515
EmilyAbrahart <emilyabrahart@gmail.com>
2019-07-12 09:29:10 +0100

    DELETE and PUT action completed, tested and working

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -2,14 +2,55 @@ const router = require('express').Router();
 const Action = require('../data/helpers/actionModel');
 
 router.get('/', (req, res) => {
-  Action.get()
-  .then(data => {
-    res.status(200).json(data);
-  })
-  .catch(error => {
-    res.status(500).json({message: "Unable to retrieve actions."})
-  })
-})
+	Action.get()
+		.then(data => {
+			res.status(200).json(data);
+		})
+		.catch(error => {
+			res.status(500).json({ message: 'Unable to retrieve actions.' });
+		});
+});
+
+router.get('/:id', validateActionId, async (req, res) => {
+	const id = req.params.id;
+
+	Action.get(id)
+		.then(data => {
+			res.status(200).json(data);
+		})
+		.catch(error => {
+			res.status(500).json({ message: `Unable to retrieve action ${id}.` });
+		});
+});
+
+router.delete('/:id', validateActionId, async (req, res) => {
+	const id = req.params.id;
+
+	Action.remove(id)
+		.then(data => {
+			res
+				.status(200)
+				.json({ message: `Action ${id} has been deleted successfully!` });
+		})
+		.catch(error => {
+			res.status(500).json({ message: `Unable to delete action ${id}` });
+		});
+});
+
+router.put('/:id', validateActionId, validateAction, async (req, res) => {
+	const id = req.params.id;
+	const { description, notes } = req.body;
+
+	Action.update(id, { description, notes })
+		.then(data => {
+			Action.get(id).then(data => {
+				res.status(200).json(data);
+			});
+		})
+		.catch(error => {
+			res.status(500).json({ message: `Unable to update action ${id}.` });
+		});
+});
 
 // Action middleware
 async function validateActionId(req, res, next) {
